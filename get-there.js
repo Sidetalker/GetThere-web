3bafe8525fe70f107ebcec402a1f1ae7b37f0d4a
Andrew <andrew.dorman.taylor@gmail.com>
2017-04-02 12:32:48 -0400

    Ridiculous PoC skeleton code

--- get-there.js
+++ get-there.js
@@ -1,6 +1,43 @@
 (function () {
 	var headingText = document.getElementById("position-hng");
 	var compass = document.getElementById("compass");
+	var lookupUserId = document.getElementById("lookupUserId");
+	var theUsersPosition = document.getElementById("theUsersPosition");
+	var firebaseMetadata = document.getElementById("firebaseMetadata");
+	var doTheLookup = document.getElementById("kk");
+	var dummyData = document.getElementById("dummyDataId");
+	var makeADummy = document.getElementById("makeADummy");
+
+    var database = firebase.database();
+
+    var randomArbitraryNumber = function(from, to) {
+    	return Math.floor(Math.random() * (to - from)) + from;
+	};
+
+    var makeAnActualDummy = function() {
+		database.ref('users/' + dummyData.value).set({
+			heading: randomArbitraryNumber(0, 359),
+			lat: randomArbitraryNumber(1, 1000),
+			lon: randomArbitraryNumber(1, 1000),
+			timestamp: new Date()
+		}).then(function() {
+			console.log("We've written to the firebase DB!");
+		});
+	};
+
+
+	var lookupUser = function() {
+		var whoWeAreCreepingOn = database.ref("users/" + lookupUserId.value);
+		whoWeAreCreepingOn.on('value', function(snapshot) {
+			theUsersPosition.textContent = "Lat: "+snapshot.val().lat + ", Lon: "+snapshot.val().lon;
+		});
+	};
+
+
+	doTheLookup.onclick = lookupUser;
+    makeADummy.onclick = makeAnActualDummy;
+
+	firebaseMetadata.textContent = "We're connected to firebase at "+firebase.app().options.databaseURL;
 
 	console.log(headingText.textContent);
 
